cbe09c45a58d31a8990bed51a5b8bd1721f4e24d
Girish Mudigonda <mudigondagirish97@gmail.com>
2024-08-18 12:06:36 +0530

    second commit

--- src/main/java/mvnlearn/Test.java
+++ src/main/java/mvnlearn/Test.java
@@ -1,14 +1,20 @@
 package mvnlearn;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
 	public static void main(String[] args) {
+//		
+//		ApplicationContext ac= new ClassPathXmlApplicationContext("config.xml");
+//		 Service sc = (Service) ac.getBean("service");
+//		 System.out.println(sc);
 		
-		ApplicationContext ac= new ClassPathXmlApplicationContext("config.xml");
-		 Service sc = (Service) ac.getBean("service");
-		 System.out.println(sc);
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(MyConfig.class);
+		GirishCreds gc = (GirishCreds) ac.getBean("gir");
+		System.out.println(gc);
+		
 	}
 }
